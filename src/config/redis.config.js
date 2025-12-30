29dd792f03fa0f44869d30d3f782d92865624d43
unknown <priyanshu123543@gmail.com>
2025-12-30 19:12:41 +0530

    Improve redis connection

--- src/config/redis.config.js
+++ src/config/redis.config.js
@@ -1,4 +1,4 @@
-import Redis from "ioredis";
+import { Redis } from "@upstash/redis";
 console.log("REDIS_URL =", process.env.REDIS_URL);
 export const redis = new Redis({
     url: process.env.UPSTASH_REDIS_REST_URL,

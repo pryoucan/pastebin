874d8f5fea8aff67cff1f42297e388abd9301418
unknown <priyanshu123543@gmail.com>
2025-12-30 18:55:22 +0530

    Add upstash REST connection

--- src/config/redis.config.js
+++ src/config/redis.config.js
@@ -1,3 +1,6 @@
 import Redis from "ioredis";
 console.log("REDIS_URL =", process.env.REDIS_URL);
-export const redis = new Redis(process.env.REDIS_URL);
+export const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
